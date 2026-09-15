9668c979bbb2c7a2c668467f67bcea9ff4eaa960
Franco Cardozo <francocardozomato@gmail.com>
2026-09-14 21:56:20 -0300

    change Wanda's name to Scarlet Witch

--- src/domain/SuperheroesList.ts
+++ src/domain/SuperheroesList.ts
@@ -7,7 +7,7 @@ export class SuperheroesList {
     { name: "Thor", side: "hero" },
     { name: "Thanos", side: "villain" },
     { name: "Loki", side: "villain" },
-    { name: "Wanda", side: "villain" },
+    { name: "Scarlet witch", side: "hero" },
     { name: "Black Widow", side: "hero" },
     { name: "Hawkeye", side: "hero" },
   ];
